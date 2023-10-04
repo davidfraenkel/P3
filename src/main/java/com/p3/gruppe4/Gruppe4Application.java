74dfb2ca58607db2a1341af59d22455165ba291d
davidfraenkel <david.fraenkel@hotmail.com>
2023-10-04 10:02:30 +0200

    Initilize spring boot

---
+++ src/main/java/com/p3/gruppe4/Gruppe4Application.java
@@ -0,0 +1,13 @@
+package com.p3.gruppe4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gruppe4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/p3/gruppe4/Gruppe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3.gruppe4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
