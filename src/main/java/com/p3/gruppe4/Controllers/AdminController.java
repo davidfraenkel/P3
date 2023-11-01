2da0ff611ea24964ca1dad08245227a7930d08cb
Anders <andersraae01@gmail.com>
2023-11-01 14:51:58 +0100

    More controller and port 3070

--- src/main/java/com/p3/gruppe4/Controllers/AdminController.java
+++ src/main/java/com/p3/gruppe4/Controllers/AdminController.java
@@ -1,9 +1,13 @@
 package com.p3.gruppe4.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
 
-    
+    @GetMapping("/hello")
+    public String hello(){
+        return "HalliHello";
+    }
 }
--- src/main/java/com/p3/gruppe4/Models/Users/Client.java
+++ src/main/java/com/p3/gruppe4/Models/Users/Client.java
@@ -2,7 +2,7 @@ package com.p3.gruppe4.Models.Users;
 
 public class Client extends User {
 
-    public Client(String username, String password) {
-        super(username, password);
+    public Client(String username, String password, String role) {
+        super(username, password, role);
     }
 }
