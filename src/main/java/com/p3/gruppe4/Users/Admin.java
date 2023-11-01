7f515c2e4e43ecb56903272a1db93d86adffc3e2
Anders <andersraae01@gmail.com>
2023-11-01 13:44:35 +0100

    Databaser med Document klasse

--- src/main/java/com/p3/gruppe4/Users/Admin.java
+++ src/main/java/com/p3/gruppe4/Users/Admin.java
@@ -1,7 +1,35 @@
 package com.p3.gruppe4.Users;
 
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class Admin extends User {
-    public Admin(String username, String password) {
-        super(username, password);
+
+    public Admin(String username, String password, String role) {
+        super(username, password, role);
     }
+
+    public void addUser(String username, String password, String role){
+        String connectionString = "mongodb+srv://pepperonis:ilovepepperonis321@p3gastrome.as1pjv9.mongodb.net/?retryWrites=true&w=majority";
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase db = mongoClient.getDatabase("Gastrome");
+
+            MongoCollection<Document> collection = db.getCollection("Users");
+            collection.insertOne(new Document().append("username", username)
+                    .append("password", password)
+                    .append("role", role)
+            );
+            System.out.println("Success! Inserted document id: ");
+
+            // Prints a message if any exceptions occur during the operation
+        } catch (MongoException me) {
+            System.err.println("Unable to insert due to an error: " + me);
+        }
+    }
+
 }
--- src/main/java/com/p3/gruppe4/Users/User.java
+++ src/main/java/com/p3/gruppe4/Users/User.java
@@ -3,11 +3,12 @@ package com.p3.gruppe4.Users;
 abstract class User {
 
     private int id;
-    private String userName, password;
+    private String userName, password, role;
 
-    public User(String username, String password){
+    public User(String username, String password, String role){
         this.userName = username;
         this.password = password;
+        this.role = role;
     }
 
     void login(String userName, String password) {
