67b14119016d15cfe72282fb070d2dc9cdeebe5b
davidfraenkel <david.fraenkel@hotmail.com>
2023-11-06 12:43:11 +0100

    test

--- src/main/java/com/p3/gruppe4/Gruppe4Application.java
+++ src/main/java/com/p3/gruppe4/Gruppe4Application.java
@@ -8,7 +8,6 @@ import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
