a4c77e760e3eb0ec5c2e929882bca405dcdbb4cb
Anders <andersraae01@gmail.com>
2023-11-06 13:54:51 +0100

    Handbook controller

--- src/main/java/com/p3/gruppe4/Models/Handbook/Handbook.java
+++ src/main/java/com/p3/gruppe4/Models/Handbook/Handbook.java
@@ -1,13 +1,16 @@
 package com.p3.gruppe4.Models.Handbook;
 
+
+import static com.mongodb.client.model.Filters.eq;
+
 import com.mongodb.MongoException;
 import com.mongodb.client.*;
+import com.mongodb.client.model.Projections;
 import org.bson.Document;
-import org.bson.types.ObjectId;
+import org.bson.conversions.Bson;
 
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
 public class Handbook {
@@ -20,12 +23,14 @@ public class Handbook {
         Set<Document> returnSet = Collections.emptySet();
         try (MongoClient mongoClient = MongoClients.create(this.connectionString)) {
             MongoDatabase db = mongoClient.getDatabase("Gastrome");
-            MongoCollection<Document> collection = db.getCollection("Topic");
+            MongoCollection<Document> collection = db.getCollection("Users");
 
             FindIterable<Document> iterableCollection = collection.find();
             Iterator iterator = iterableCollection.iterator();
 
             while (iterator.hasNext()) {
+                System.out.println("Next");
+                System.out.println(iterator.next());
                 returnSet.add((Document) iterator.next());
             }
             // Prints a message if any exceptions occur during the operation
@@ -35,7 +40,24 @@ public class Handbook {
         return returnSet;
     }
 
-    public Document getTopic(String name){}
+    public Document getTopic(String topicName){
+        Document returnDocument = new Document();
+        try (MongoClient mongoClient = MongoClients.create(this.connectionString)) {
+            MongoDatabase db = mongoClient.getDatabase("Gastrome");
+            MongoCollection<Document> collection = db.getCollection("Topic");
+
+            Bson projectionFields = Projections.fields(
+                    Projections.include("name"));
+
+            returnDocument = collection.find(eq("name", topicName))
+                    .first();
+
+            // Prints a message if any exceptions occur during the operation
+        } catch (MongoException me) {
+            System.err.println("Unable to insert due to an error: " + me);
+        }
+        return returnDocument;
+    }
 
     public void manageTopic(){
 //        switch (operation){
---
+++ src/test/java/com/p3/gruppe4/Models/Handbook/HandbookTest.java
@@ -0,0 +1,24 @@
+package com.p3.gruppe4.Models.Handbook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandbookTest {
+
+    @Test
+    void getAllTopics() {
+    }
+
+    @Test
+    void getTopic() {
+    }
+
+    @Test
+    void manageTopic() {
+    }
+
+    @Test
+    void manageSubTopic() {
+    }
+}
