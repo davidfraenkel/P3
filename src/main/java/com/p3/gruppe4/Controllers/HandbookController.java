811444e0a70f1e4aaf6d4afc1c6c734f089ee133
Anders <andersraae01@gmail.com>
2023-11-08 10:20:13 +0100

    crossorigin 3000;

--- src/main/java/com/p3/gruppe4/Controllers/HandbookController.java
+++ src/main/java/com/p3/gruppe4/Controllers/HandbookController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class HandbookController {
     Handbook handbook = new Handbook();
