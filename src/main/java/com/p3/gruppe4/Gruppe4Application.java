8e15c14cef3da85ee6ea11032f00c2955795aa81
Anders <andersraae01@gmail.com>
2023-11-14 13:30:02 +0100

    fix main method

--- src/main/java/com/p3/gruppe4/Gruppe4Application.java
+++ src/main/java/com/p3/gruppe4/Gruppe4Application.java
@@ -8,11 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Gruppe4Application {
 
 	public static void main(String[] args) {
-		// Database setup
-		Admin newAdmin = new Admin("Admin user", "password", "Admin");
-
-		newAdmin.addContentCreator("User 1", "password123");
-
 		// Spring Boot run
 		SpringApplication.run(Gruppe4Application.class, args);
 	}
