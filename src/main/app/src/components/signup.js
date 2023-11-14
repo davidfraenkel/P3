a62e0fcf4b80cdb1712f792a8d5969531c6688da
Gustaw <gustaw1313@gmail.com>
2023-11-14 13:16:34 +0100

    gty

--- src/main/app/src/components/signup.js
+++ src/main/app/src/components/signup.js
@@ -13,8 +13,23 @@ export default function Signup() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(inputs);
+
+        fetch('http://localhost:3002/api/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(inputs),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     };
+
 
     return (
         <div className="SignupBackgroundImg">
