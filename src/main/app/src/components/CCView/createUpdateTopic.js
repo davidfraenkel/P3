50bab78ba4e2e7d53eaf8e68c5562f74e562e2c9
davidfraenkel <david.fraenkel@hotmail.com>
2023-11-29 09:28:14 +0100

    Createtopic now works

--- src/main/app/src/components/CCView/createUpdateTopic.js
+++ src/main/app/src/components/CCView/createUpdateTopic.js
@@ -1,72 +1,67 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import {Link, useLocation} from "react-router-dom";
 import InputField from '../smartComponents/inputField';
 import '../CCView/styling/createUpdateTopic.css';
 
 export default function CreateUpdateTopic(props)  {
-    const [topicTitle, setTopicTitle] = useState('');
-    const [file, setFile] = useState(null);
-    const location = useLocation();
-    const searchParams= new URLSearchParams(location.search);
-    const topicName = searchParams.get('topicName');
+    const [name, setName] = useState('');
+    const [imagePath, setImagePath] = useState('');
+    const [image, setImage] = useState(null);
 
-    const handleTitleChange = (e) => {
-        setTopicTitle(e.target.value);
+    const handleNameChange = (e) => {
+        setName(e.target.value);
     };
 
-    const handleFileChange = (e) => {
-        const selectedFile = e.target.files[0];
-        setFile(selectedFile);
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+    };
+    const handlePhoneNumberChange = (e) => {
+        setImagePath(e.target.value);
     };
 
-    const handleDrop = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const selectedFile = e.dataTransfer.files[0];
-        setFile(selectedFile);
-    };
+        const formData = new FormData();
+        formData.append('image', image);
 
-    const preventDefault = (e) => {
-        e.preventDefault();
-    };
+        const topicData = {
+            name: name,
+            imagePath: imagePath,
+        };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle topic creation/update logic here, including file upload.
-        console.log('Topic Title:', topicTitle);
-        console.log('Selected File:', file);
+        formData.append('topic', JSON.stringify(topicData));
+
+        try {
+            const response = await fetch('http://localhost:3002/api/createTopic', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                console.log('Topic created successfully');
+            } else {
+                console.error('Failed to create topic');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
-        <div className="FormCreateUpdateTopicBackgroundOverlay">
-            <div className="FormCreateUpdateTopicContainer">
-                <h1 className="FormCreateUpdateTopicTitle">{topicName ? `Update ${topicName}` : 'Create new topic'}</h1>
-                <form onSubmit={handleSubmit} className="FormCreateUpdateTopicForm">
-                    <div className="FormCreateUpdateTopicInputContainer">
-                        <InputField
-                            type={"text"}
-                            name={"topic"}
-                            placeholder={"Topic Name"}
-                            value={topicTitle}
-                            func={handleTitleChange}
-                        />
-                    </div>
-                    <div
-                        className="FormCreateUpdateTopicDropArea"
-                        onDrop={handleDrop}
-                        onDragOver={preventDefault}
-                        onDragEnter={preventDefault}
-                    >
-                        <label className="FormCreateUpdateTopicLabel">Drag and Drop Image Here:</label>
-                        <input type="file" accept="image/*" onChange={handleFileChange} className="FormCreateUpdateTopicFileInput" />
-                    </div>
-                    {file && (
-                        <p className="FormCreateUpdateTopicFileName">Selected File: {file.name}</p>
-                    )}
-
-                    <button type="submit" className="FormCreateUpdateTopicSubmitButton">Submit</button>
-                </form>
-            </div>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input type="text" value={name} onChange={handleNameChange} />
+                <input type="text" value={imagePath} onChange={handlePhoneNumberChange} />
+            </label>
+            <br />
+            <label>
+                Image:
+                <input type="file" accept="image/*" onChange={handleImageChange} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
     );
 };
-
