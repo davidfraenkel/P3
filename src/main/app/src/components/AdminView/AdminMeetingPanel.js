d44aa5205b5e78ee37a136dac7f3a73f5cd998d8
Kasper-Sie <117650279+Kasper-Sie@users.noreply.github.com>
2023-12-06 14:06:38 +0100

    Here you go anders

--- src/main/app/src/components/AdminView/AdminMeetingPanel.js
+++ src/main/app/src/components/AdminView/AdminMeetingPanel.js
@@ -1,7 +1,20 @@
 export default function AdminMeetingPanel() {
     return (
+
         <div>
             <h1>Admin Meeting Panel</h1>
+
+            <div className="Pending">
+                <div className="MeetingBoxContainer">
+                    
+                </div>
+            </div>
+            <div className="Accepted">
+                <div className="MeetingBoxContainer">
+
+                </div>
+
+            </div>
         </div>
     )
 }
